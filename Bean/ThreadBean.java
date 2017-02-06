ffe25921672c76641c759fd9d0cad9df3112e067
H16304005\a <to16304005@ga.tera-house.ac.jp>
2017-02-06 14:33:11 +0900

    Beanを前のクラス図をもとに作ったので、編集よろ

---
+++ Bean/ThreadBean.java
@@ -0,0 +1,34 @@
+package pac;
+
+import java.util.ArrayList;
+
+public class ThreadBean{
+
+  private int threadId;
+  private String threadName;
+  private String threadCreatedDate;
+  private ArrayList resBean;
+
+  public int getThreadId(){
+    return this.threadId;
+  }
+  public String getThreadName(){
+    return this.threadName;
+  }
+  public String getThreadCreatedDate(){
+    return this.threadCreatedDate;
+  }
+  public ArrayList getResBean(){
+    return this.resBean;
+  }
+
+  public void setThreadId(int threadId){
+    this.threadId = threadId;
+  }
+  public void setThreadName(String threadName){
+    this.threadName = threadName;
+  }
+  public void setThreadCreatedDate(String threadCreatedDate){
+    this.threadCreatedDate = threadCreatedDate;
+  }
+}
